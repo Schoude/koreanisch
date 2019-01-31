b2fc75c256e8169070fb312142fc50316b66c028
Marc B <marcbaque1311@web.de>
2019-01-31 03:06:27 +0100

    .

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,43 +3,38 @@ module.exports = {
 	description:
 		'Eine (derzeit) lose Zusammenstellung an koreanischer Grammatik mit Beispielsätzen.',
 	themeConfig: {
-		serviceWorker: {
-			updatePopup: {
-				message: 'Es sind neue Inhalte verfügbar!',
-				buttonText: 'Neu laden!'
-			}
+		serviceWorker: true
+	},
+	nav: [
+		{
+			text: 'Home',
+			link: '/'
 		},
-		nav: [
-			{
-				text: 'Home',
-				link: '/'
-			},
-			{
-				text: 'Kategorien',
-				link: '/kategorien/',
-				items: [
-					{
-						text: 'Abfolgende Handlungen',
-						link: '/abfolgende-handlungen'
-					},
-					{
-						text: 'Tempi',
-						items: [
-							{ text: 'Präsens', link: '/tempi/präsens' },
-							{ text: 'Präteritum', link: '/tempi/präteritum' }
-						]
-					}
-				]
-			},
-			{
-				text: 'Über den Autor',
-				link: '/autor.html'
-			}
-		],
-		/* 'auto' */
-
-		sidebar: {
-			'/kategorien/': ['', 'präsens', 'präteritum']
+		{
+			text: 'Kategorien',
+			link: '/kategorien/',
+			items: [
+				{
+					text: 'Abfolgende Handlungen',
+					link: '/abfolgende-handlungen'
+				},
+				{
+					text: 'Tempi',
+					items: [
+						{ text: 'Präsens', link: '/tempi/präsens' },
+						{ text: 'Präteritum', link: '/tempi/präteritum' }
+					]
+				}
+			]
+		},
+		{
+			text: 'Über den Autor',
+			link: '/autor.html'
 		}
+	],
+	/* 'auto' */
+
+	sidebar: {
+		'/kategorien/': ['', 'präsens', 'präteritum']
 	}
 }
